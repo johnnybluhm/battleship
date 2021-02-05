2a9cb9d1b109cc8a5cf3f1ee7798205093c32ec6
johnnybluhm <54964159+johnnybluhm@users.noreply.github.com>
2021-02-04 19:54:37 -0700

    fixed issue

--- src/edu/colorado/exhalation/Ship.java
+++ src/edu/colorado/exhalation/Ship.java
@@ -6,7 +6,7 @@ public class Ship {
     // TODO: create appropriate getter and setter methods
     // TODO: Understand encapsulation
     // TODO: Understand what these todo comments mean
-    //team exhalation, pari 1 was here
+    //team exhalation, pair 1 was here
     public  void show()     {     // dunno why this is here maybe it is just an example method
         System.out.println("IF you can't see this then something is severely wrong!!");
     }
