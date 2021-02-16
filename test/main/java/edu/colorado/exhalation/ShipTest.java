8dd9301d7378dd3e84ed57f68b60b055f7ae44ee
johnnybluhm <54964159+johnnybluhm@users.noreply.github.com>
2021-02-15 23:12:29 -0700

    tried building a test
    It worked

---
+++ test/main/java/edu/colorado/exhalation/ShipTest.java
@@ -0,0 +1,14 @@
+package main.java.edu.colorado.exhalation;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShipTest {
+
+    @Test
+    void testobj() {
+        assertEquals(1,1);
+    }
+}
