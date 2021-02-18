36aad5870602480af0c2ee22cfaf480c33fdfccb
johnnybluhm <54964159+johnnybluhm@users.noreply.github.com>
2021-02-17 17:42:51 -0700

    finished one test. added another

--- src/main/java/edu/colorado/exhalation/Peg.java
+++ src/main/java/edu/colorado/exhalation/Peg.java
@@ -4,6 +4,7 @@ public class Peg {
 
     private int pos_x_;
     private int pos_y_;
+    private int type_;
     public Peg(int pos_x, int pos_y){
         this.pos_x_ = pos_x;
         this.pos_y_ = pos_y;
@@ -23,5 +24,13 @@ public class Peg {
 
     public void setPos_y_(int pos_y_) {
         this.pos_y_ = pos_y_;
+    }
+
+    public int getType_() {
+        return type_;
+    }
+
+    public void setType_(int type) {
+        this.type_ = type;
     }
 }
--- test/main/java/edu/colorado/exhalation/PegTest.java
+++ test/main/java/edu/colorado/exhalation/PegTest.java
@@ -8,20 +8,29 @@ import static org.junit.jupiter.api.Assertions.*;
 class PegTest {
 
 
+    private Peg test_peg;
 
-    //will change once constructor is added
+
+    @BeforeEach
+    void create(){
+      test_peg = new Peg(1,0);
+    }
+
+    //test using point (1,0)
     @Test
     void testCreate(){
-        Peg test_peg = new Peg(1,0);
         assertEquals(1, test_peg.getPos_x_());
         assertEquals(0, test_peg.getPos_y_());
     }
     @Test
-    voidTestPegType(){
-        Peg test_peg = new Peg(1,0);
+    void testPegType(){
         test_peg.setType_(1);
         assertEquals(1, test_peg.getType_());
+    }
 
+    @Test
+    void testHit(){
+        
     }
 
 
