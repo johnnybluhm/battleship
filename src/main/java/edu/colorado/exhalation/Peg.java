591fb234cc7058b3bd0f4c72244939c18310edc7
johnnybluhm <54964159+johnnybluhm@users.noreply.github.com>
2021-02-17 17:03:38 -0700

    added pegTest iteration1

---
+++ src/main/java/edu/colorado/exhalation/Peg.java
@@ -0,0 +1,6 @@
+package main.java.edu.colorado.exhalation;
+
+public class Peg {
+
+
+}
---
+++ test/main/java/edu/colorado/exhalation/PegTest.java
@@ -0,0 +1,22 @@
+package main.java.edu.colorado.exhalation;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PegTest {
+
+    //will change once constructor is added
+    @Test
+    void testCreate(){
+        new Peg();
+    }
+
+
+    @Test
+    void testPosition{
+
+
+    }
+
+}
