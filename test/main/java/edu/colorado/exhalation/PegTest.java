93b7e306b4914709472d175a6eddf64a1d50c471
johnnybluhm <54964159+johnnybluhm@users.noreply.github.com>
2021-02-17 17:08:23 -0700

    added Peg constructor test

--- test/main/java/edu/colorado/exhalation/PegTest.java
+++ test/main/java/edu/colorado/exhalation/PegTest.java
@@ -1,22 +1,21 @@
 package main.java.edu.colorado.exhalation;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PegTest {
 
+    private Peg test_peg;
+
     //will change once constructor is added
     @Test
     void testCreate(){
-        new Peg();
+        test_peg = new Peg(1,0);
+        assertEquals(1, test_peg.getPostition_x());
+        assertEquals(0, test_peg.getPostition_y());
     }
 
-
-    @Test
-    void testPosition{
-
-
-    }
 
 }
