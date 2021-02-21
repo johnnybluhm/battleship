6b578a6219b7a1932aef033990962f40dfc9e71d
johnnybluhm <54964159+johnnybluhm@users.noreply.github.com>
2021-02-21 12:16:29 -0700

    test passed

--- src/main/java/edu/colorado/exhalation/Point.java
+++ src/main/java/edu/colorado/exhalation/Point.java
@@ -1,4 +1,30 @@
 package main.java.edu.colorado.exhalation;
 
 public class Point {
+
+    private int x_;
+    private int y_;
+
+    public Point(int x, int y){
+        this.x_ = x;
+        this.y_ = y;
+    }
+
+
+    public int getX_() {
+        return x_;
+    }
+
+    public void setX_(int x_) {
+        this.x_ = x_;
+    }
+
+    public int getY_() {
+        return y_;
+    }
+
+    public void setY_(int y_) {
+        this.y_ = y_;
+    }
 }
+
