7ad3aaacdfb26da9f609d706ffc733ee531f76cc
Ethan Bilek <etbi8014@colorado.edu>
2021-02-21 01:16:14 -0700

    Added test for Ship class to see if it has sunk

--- test/main/java/edu/colorado/exhalation/ShipTest.java
+++ test/main/java/edu/colorado/exhalation/ShipTest.java
@@ -1,5 +1,7 @@
 package main.java.edu.colorado.exhalation;
 
+import edu.colorado.exhalation.Peg;
+import edu.colorado.exhalation.Ship;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -8,7 +10,20 @@ import static org.junit.jupiter.api.Assertions.*;
 class ShipTest {
 
     @Test
-    void testobj() {
-        assertEquals(1,1);
+    void testShipSunk() {
+        Peg ship1 = new Peg(1, 0, 2);
+        Peg ship2 = new Peg(1, 1, 2);
+        Peg ship3 = new Peg(1, 2, 3);
+        Peg ship4 = new Peg(1, 3, 4);
+        ship1.hit();
+        ship2.hit();
+        ship3.hit();
+        Peg [] location = {ship1, ship2};
+        Peg [] location2 = {ship3, ship4};
+        Ship mine1 = new Ship(location);
+        Ship mine2 = new Ship(location2);
+
+        assertEquals(true, mine1.isSunk());
+        assertEquals(false, mine2.isSunk());
     }
 }
