d6bf4ebda014947956b99aa8174a352fbe429c0e
etbi8014 <etbi8014@colorado.edu>
2021-03-20 04:10:50 -0600

    Example of how Command Pattern could be used in Main.java

--- src/main/java/edu/colorado/exhalation/Main.java
+++ src/main/java/edu/colorado/exhalation/Main.java
@@ -7,5 +7,19 @@ public class Main {
         //change
          //Ship ship = new Ship();
          //ship.show();
+        /* Example of how main class might be written with Command pattern
+        Game battleShip = new Game();
+        Board temp = new Board();
+        battleShip.setBoard(temp);
+
+        PlayerAction playerActions = new PlayerAction();
+        playerActions.SetCommand(0, new MoveShips(battleShip));
+        playerActions.SetCommand(1, new Hit(battleShip));
+
+        playerActions.Move(0, 'E');
+        playerActions.Undo();
+        playerActions.Redo();
+        playerActions.Action(1, new Point(1, 1));
+        */
     }
 }
