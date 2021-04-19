11e57b4bb20548a434e50975e7fe04c94f3342c7
johnnybluhm <54964159+johnnybluhm@users.noreply.github.com>
2021-04-19 14:49:43 -0600

    storm passing tests

--- src/main/java/edu/colorado/exhalation/Storm.java
+++ src/main/java/edu/colorado/exhalation/Storm.java
@@ -3,16 +3,50 @@ package edu.colorado.exhalation;
 public class Storm {
 
     private Point[] points_;
-    private int storm_size_;
+    private int size_;
 
     public Storm(){
 
+        int random_num = (int)(Math.random()*100 % 10);
+        int storm_size;
+        if(random_num <3){
+            //30%
+            storm_size = 2;
+            this.size_ = storm_size;
+            Point[] points = generatePoints(storm_size);
+            while(points == null){
+                points = generatePoints(storm_size);
+            }
+            this.points_ = points;
+        }
+        else if(random_num >= 3 && random_num<=6){
+            //40%
+            storm_size =3;
+            this.size_ = storm_size;
+            Point[] points = generatePoints(storm_size);
+            while(points == null){
+                points = generatePoints(storm_size);
+            }
+            this.points_ = points;
+        }
+        else if(random_num >6 && random_num<=9){
+            //30%
+            storm_size = 4;
+            this.size_ = storm_size;
+            Point[] points = generatePoints(storm_size);
+            while(points == null){
+                points = generatePoints(storm_size);
+            }
+            this.points_ = points;
+        }
+    }
+
+    //-1 storm had invalid point in it
+    public static Point[] generatePoints(int storm_size){
         int random_x;
         int random_y;
         Point random_point;
-        int random_num;
 
-        random_num = (int)(Math.random()*100 % 10);
         random_x = (int)(Math.random()*100 % 10);
         random_y = (int)(Math.random()*100 % 10);
         random_point = new Point(random_x,random_y);
@@ -23,114 +57,87 @@ public class Storm {
             random_y = (int)(Math.random()*100 % 10);
             random_point = new Point(random_x,random_y);
         }
-        if(random_num <3){
-            //30%
-            this.storm_size_ = 2;
-        }
-        else if(random_num >= 3 && random_num<6){
-            //30%
-            storm_size_ =3;
-        }
-        else if(random_num >=6 && random_num<8){
-            //20%
-            storm_size_ = 4;
-        }
-        else if(random_num >= 8 && random_num<9){
-            //10%
-            storm_size_ = 5;
-        }
-        else{
-            //10%
-            storm_size_ = 6;
-        }
-    }
+        int x = random_x;
+        int y = random_y;
 
-    public static void generatePoints(Point start_point, int storm_size){
-        int x = start_point.getX();
-        int y = start_point.getY();
         if(storm_size == 2){
-            Point[] points = new Point[storm_size+1];
+
+            Point[] points = new Point[storm_size*storm_size];
             //get points to sides
-            points[0] = new Point(x+1,y);
-            points[1] = new Point(x+2,y);
-            points[2] = new Point(x-1,y);
-            points[3] = new Point(x-2,y);
+            points[0] = random_point;
+            points[1] = new Point(x+1,y);
+            points[2] = new Point(x+1,y+1);
+            points[3] = new Point(x,y+1);
 
-            //get points above/below
-            points[4] = new Point(x,y+1);
-            points[5] = new Point(x,y+2);
-            points[6] = new Point(x,y-1);
-            points[7] = new Point(x,y-2);
-
-            //get corners
-            points[8] = new Point(x+1,y+1);
-            points[9] = new Point(x-1,y-1);
-            points[10] = new Point(x-1,y+1);
-            points[11] = new Point(x+1,y-1);
-
-            //add selected point
-            //points[12] = point;
+            for (int i = 0; i <points.length ; i++) {
+                if(!points[i].isValid()){
+                    return null;
+                }
+            }
+            return points;
         }
         else if(storm_size == 3){
-            Point[] points = new Point[storm_size+1];
-            //get points to sides
-            points[0] = new Point(x+1,y);
-            points[1] = new Point(x+2,y);
-            points[2] = new Point(x-1,y);
-            points[3] = new Point(x-2,y);
 
-            //get points above/below
-            points[4] = new Point(x,y+1);
+            Point[] points = new Point[storm_size*storm_size];
+
+            points[0] = random_point;
+            points[1] = new Point(x+1,y);
+            points[2] = new Point(x+1,y+1);
+            points[3] = new Point(x,y+1);
+
+
+            points[4] = new Point(x+2,y);
             points[5] = new Point(x,y+2);
-            points[6] = new Point(x,y-1);
-            points[7] = new Point(x,y-2);
+            points[6] = new Point(x+2,y+2);
+            points[7] = new Point(x+1,y+2);
+            points[8] = new Point(x+2,y+1);
 
-            //get corners
-            points[8] = new Point(x+1,y+1);
-            points[9] = new Point(x-1,y-1);
-            points[10] = new Point(x-1,y+1);
-            points[11] = new Point(x+1,y-1);
-
-            //add selected point
-            //points[12] = point;
+            for (int i = 0; i <points.length ; i++) {
+                if(!points[i].isValid()){
+                    return null;
+                }
+            }
+            return points;
         }
         else if(storm_size == 4){
-            Point[] points = new Point[storm_size+1];
-            //get points to sides
-            points[0] = new Point(x+1,y);
-            points[1] = new Point(x+2,y);
-            points[2] = new Point(x-1,y);
-            points[3] = new Point(x-2,y);
 
-            //get points above/below
-            points[4] = new Point(x,y+1);
+            Point[] points = new Point[storm_size*storm_size];
+
+            points[0] = random_point;
+            points[1] = new Point(x+1,y);
+            points[2] = new Point(x+1,y+1);
+            points[3] = new Point(x,y+1);
+
+
+            points[4] = new Point(x+2,y);
             points[5] = new Point(x,y+2);
-            points[6] = new Point(x,y-1);
-            points[7] = new Point(x,y-2);
+            points[6] = new Point(x+2,y+2);
+            points[7] = new Point(x+1,y+2);
+            points[8] = new Point(x+2,y+1);
 
-            //get corners
-            points[8] = new Point(x+1,y+1);
-            points[9] = new Point(x-1,y-1);
-            points[10] = new Point(x-1,y+1);
-            points[11] = new Point(x+1,y-1);
+            points[9] = new Point(x+3,y);
+            points[10] = new Point(x,y+3);
+            points[11] = new Point(x+2,y+3);
+            points[12] = new Point(x+3,y+2);
+            points[13] = new Point(x+3,y+1);
+            points[14] = new Point(x+1,y+3);
+            points[15] = new Point(x+3,y+3);
 
-            //add selected point
-            //points[12] = point;
+            for (int i = 0; i <points.length ; i++) {
+                if(!points[i].isValid()){
+                    return null;
+                }
+            }
+            return points;
         }
-        else if(storm_size == 5){
-
-        }
-    }
-
-    public int getStormSize(){
-        return this.storm_size_;
+        return null;
     }
 
     public Point[] getPoints(){
         return this.points_;
     }
 
-    public boolean containsPeg(Peg peg){
+    public boolean includes(Peg peg){
         Point peg_point = peg.getPoint();
         for (int i = 0; i <this.getPoints().length ; i++) {
             if(this.getPoints()[i].equals(peg_point)){
@@ -138,5 +145,9 @@ public class Storm {
             }
         }
         return false;
+    }
+
+    public int getSize(){
+        return this.size_;
     }
 }
--- src/test/java/edu/colorado/exhalation/StormTest.java
+++ src/test/java/edu/colorado/exhalation/StormTest.java
@@ -9,12 +9,40 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StormTest {
     @Test
-    void testContainsPeg(){
+    void testIncludesPeg(){
         Storm storm = new Storm();
         Board board = new Board();
         Point test_point = storm.getPoints()[0];
 
         Peg peg = board.getPeg(test_point);
-        //Assertions.assertTrue(storm.hasPeg(peg));
+        Assertions.assertTrue(storm.includes(peg));
+    }
+
+    @Test
+    void testProbabilityDistribution(){
+
+        Storm storm;
+        int[] size_count = new int[3];
+        size_count[0] = 0;//30%
+        size_count[1] = 0;//40%
+        size_count[2] = 0;//30%
+        for (int i = 0; i < 10000; i++) {
+            storm = new Storm();
+            if (storm.getSize() == 2) {
+                size_count[0] += 1;
+            } else if (storm.getSize() == 3) {
+                size_count[1] += 1;
+            } else if (storm.getSize() == 4) {
+                size_count[2] += 1;
+            }
+        }
+            int size_two = size_count[0];
+            int size_three = size_count[1];
+            int size_four = size_count[2];
+
+            //+-150 for error
+            Assertions.assertTrue(size_two>2850 && size_two <3150);
+            Assertions.assertTrue(size_three>3850 && size_three <4150);
+            Assertions.assertTrue(size_four>2850 && size_four <3150);
     }
 }
